0a2588806bbc58cac9f2b03424e2b7c70d68d5be
_ID <jc271200001@protonmail.com>
2021-04-10 17:58:32 +0200

    train the model

--- script.js
+++ script.js
@@ -36,19 +36,30 @@ async function run() {
       height: 300,
     }
   );
-  // More code will be added below
+  // Create the model
+  const model = createModel();
+  tfvis.show.modelSummary({ name: "Model Summary" }, model);
+  // Convert the data to a form we can use for training.
+  const tensorData = convertToTensor(data);
+  const { inputs, labels } = tensorData;
+
+  // Train the model
+  await trainModel(model, inputs, labels);
+  console.log("Done Training");
 }
 
 document.addEventListener("DOMContentLoaded", run);
 
 function createModel() {
-  tfvis.show.modelSummary({ name: "Model Summary" }, model);
   // Create a sequential model
   const model = tf.sequential();
+
   // Add a single input layer
   model.add(tf.layers.dense({ inputShape: [1], units: 1, useBias: true }));
+
   // Add an output layer
   model.add(tf.layers.dense({ units: 1, useBias: true }));
+
   return model;
 }
 
@@ -94,5 +105,28 @@ function convertToTensor(data) {
       labelMax,
       labelMin,
     };
+  });
+}
+
+async function trainModel(model, inputs, labels) {
+  // Prepare the model for training.
+  model.compile({
+    optimizer: tf.train.adam(),
+    loss: tf.losses.meanSquaredError,
+    metrics: ["mse"],
+  });
+
+  const batchSize = 32;
+  const epochs = 50;
+
+  return await model.fit(inputs, labels, {
+    batchSize,
+    epochs,
+    shuffle: true,
+    callbacks: tfvis.show.fitCallbacks(
+      { name: "Training Performance" },
+      ["loss", "mse"],
+      { height: 200, callbacks: ["onEpochEnd"] }
+    ),
   });
 }
